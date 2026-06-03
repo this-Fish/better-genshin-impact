56f8c4377d37d0531db7f57394053dc96f93a6cb
this-Fish <york5252@gmail.com>
2026-06-03 12:17:31 +0800

    修正: 移除不必要的 HoldFinishMode 熱鍵模式

--- BetterGenshinImpact/ViewModel/Pages/MacroSettingsPageViewModel.cs
+++ BetterGenshinImpact/ViewModel/Pages/MacroSettingsPageViewModel.cs
@@ -18,8 +18,8 @@ public partial class MacroSettingsPageViewModel : ViewModel
     private readonly INavigationService _navigationService;
 
     [ObservableProperty]
-    private string[] _quickFightMacroHotkeyMode = [OneKeyFightTask.HoldOnMode, OneKeyFightTask.HoldFinishMode, OneKeyFightTask.TickMode];
-
+    private string[] _quickFightMacroHotkeyMode = [OneKeyFightTask.HoldOnMode, OneKeyFightTask.TickMode];
+    
     public MacroSettingsPageViewModel(IConfigService configService, INavigationService navigationService)
     {
         Config = configService.Get();
